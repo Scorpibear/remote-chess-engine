3a7145543dd19d100f23b070294befd4b082b05f
Mikhail Subach <scorpibear@tut.by>
2017-11-24 09:13:24 +0300

    RICPA v0.1 stub implementation

--- server.js
+++ server.js
@@ -3,19 +3,23 @@ app = express(),
 port = process.env.PORT || 9977;
 
 app.get('/', (req, res) => {
-  res.send('<a href="https://raw.githubusercontent.com/Scorpibear/ricpa-specification/master/README.md">Please, check specification for supported API methods</a>');
+  res.send('This remote chess engine supports <a href="https://github.com/Scorpibear/ricpa-specification/releases/tag/v0.1">RICPA specification</a>. Check it for supported API methods.');
 });
 
 app.get('/fen', (req, res) => {
-  res.send(JSON.stringify({"status": "Not implemented yet"}));
+  res.send(JSON.stringify({"bestmove": undefined, "estimatedTime": undefined}));
+});
+
+app.post('/fen', (req, res) => {
+  res.send('Not implemented yet');
+});
+
+app.delete('/fen', (req, res) => {
+  res.send('Not implemented yet');
 });
 
 app.get('/queue', (req, res) => {
-  res.send('Not implemented yet')
-});
-
-app.put('/fen', (req, res) => {
-  res.send('Not implemented yet')
+  res.send('Not implemented yet');
 });
 
 app.listen(port);
