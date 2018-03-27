86ad9ecdf56e6465a314b1c79130bd8e2c7f799f
Mikhail Subach <scorpibear@tut.by>
2018-03-27 17:47:20 +0300

    evaluations save and get

--- evaluations.js
+++ evaluations.js
@@ -1,6 +1,9 @@
-exports.get = () => {
+const data = new Map();
 
-};
+exports.get = ({ fen }) => data.get(fen);
 
-exports.save = ({ fen, depth, bestMove }) => {
+exports.save = ({
+  fen, depth, bestMove, score
+}) => {
+  data.set(fen, { depth, bestMove, score });
 };
--- spec/evaluations-spec.js
+++ spec/evaluations-spec.js
@@ -1,9 +1,17 @@
+const evaluations = require('../evaluations');
+
 describe('evaluations', () => {
   describe('get', () => {
-    it('returns saved evaluation');
-    it('returns undefined for unknown evaluations');
+    it('returns undefined for unknown evaluations', () => {
+      expect(evaluations.get({ fen: 'abc' })).toBeUndefined();
+    });
   });
   describe('save', () => {
-    it('saves evaluation');
+    it('saves evaluation', () => {
+      evaluations.save({
+        fen: 'aaa', depth: 50, bestMove: 'Nf3', score: 0.23
+      });
+      expect(evaluations.get({ fen: 'aaa' })).toEqual({ depth: 50, bestMove: 'Nf3', score: 0.23 });
+    });
   });
 });
