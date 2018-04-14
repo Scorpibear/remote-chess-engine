8d43d5644ddd47ddbe9ba5c32fc9810013a26bea
Mikhail Subach <scorpibear@tut.by>
2018-04-14 12:04:12 +0300

    additional tests to serializer

--- spec/serializer-spec.js
+++ spec/serializer-spec.js
@@ -1,3 +1,6 @@
+const config = require('config');
+const fs = require('fs');
+
 const serializer = require('../serializer');
 
 describe('serializer', () => {
@@ -11,7 +14,30 @@ describe('serializer', () => {
       serializer.serialize(serializable, 'not-existent-file');
       expect(serializable.load).not.toHaveBeenCalled();
     });
-    it('calls load with output of file');
-    it('writes to file on change');
+    it('calls load with output of file as object', () => {
+      spyOn(fs, 'readFileSync').and.returnValue('{"a":70}');
+      spyOn(fs, 'existsSync').and.returnValue(true);
+      spyOn(serializable, 'load');
+      serializer.serialize(serializable, 'some-file');
+      expect(serializable.load).toHaveBeenCalledWith({ a: 70 });
+    });
+    it('writes to file on change', () => {
+      spyOn(fs, 'writeFile').and.stub();
+      let onChange;
+      spyOn(serializable, 'on').and.callFake((event, callback) => { onChange = callback; });
+      serializer.serialize(serializable, 'test-file');
+
+      onChange();
+
+      expect(fs.writeFile).toHaveBeenCalled();
+    });
+  });
+  describe('serializeAll', () => {
+    it('calls serialize()', () => {
+      spyOn(config, 'get').and.returnValue('filename.json');
+      spyOn(serializer, 'serialize').and.stub();
+      serializer.serializeAll();
+      expect(serializer.serialize).toHaveBeenCalledWith(jasmine.anything(), 'filename.json');
+    });
   });
 });
