2094cd6b795328cac0a9977db6a23ea975cdcc1f
Mikhail Subach <scorpibear@tut.by>
2018-10-18 08:34:17 +0300

    evaluations.get and .save consider depths

--- spec/evaluations-spec.js
+++ spec/evaluations-spec.js
@@ -1,32 +1,49 @@
 const Evaluations = require('../app/evaluations');
 
 describe('evaluations', () => {
-  const evaluations = new Evaluations();
+  let evaluations;
+  const fen = 'abc';
+  const depth = 100;
+  const bestMove = 'Nf6';
+  const score = -0.2;
+  const data = {
+    fen, depth, bestMove, score
+  };
+  beforeEach(() => {
+    evaluations = new Evaluations();
+  });
   describe('get', () => {
     it('returns undefined for unknown evaluations', () => {
-      expect(evaluations.get({ fen: 'abc' })).toBeUndefined();
+      expect(evaluations.get({ fen })).toBeUndefined();
+    });
+    it('returns undefined for higher depth', () => {
+      evaluations.save(data);
+      expect(evaluations.get({ fen, depth: depth + 2 })).toBeUndefined();
     });
   });
   describe('load', () => {
     it('loads evaluations', () => {
-      const data1 = { depth: 100, bestMove: 'e4', score: 1.23 };
-      evaluations.load([['abc', data1]]);
-      expect(evaluations.getAllData()).toEqual([['abc', data1]]);
+      const data1 = { depth, bestMove, score };
+      evaluations.load([[fen, data1]]);
+      expect(evaluations.getAllData()).toEqual([[fen, data1]]);
     });
   });
   describe('save', () => {
     it('saves evaluation', () => {
-      evaluations.save({
-        fen: 'aaa', depth: 50, bestMove: 'Nf3', score: 0.23
-      });
-      expect(evaluations.get({ fen: 'aaa' })).toEqual({ depth: 50, bestMove: 'Nf3', score: 0.23 });
+      evaluations.save(data);
+      expect(evaluations.get({ fen })).toEqual({ depth, bestMove, score });
     });
     it('emits change event', () => {
       spyOn(evaluations, 'emitChangeEvent');
-      evaluations.save({
-        fen: 'aaa', depth: 100, bestMove: 'Nf3', score: 0.23
-      });
+      evaluations.save(data);
       expect(evaluations.emitChangeEvent).toHaveBeenCalled();
+    });
+    it('leaves current data if trying to save with lower depth', () => {
+      evaluations.save(data);
+      evaluations.save({
+        fen, depth: depth - 2, bestMove, score
+      });
+      expect(evaluations.get({ fen })).toEqual({ depth, bestMove, score });
     });
   });
 });
